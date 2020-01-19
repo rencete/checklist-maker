78a949ab5f4574938496ffad4359f87db3c39c3c
Terence Yu <rencete.workonline@gmail.com>
2020-01-19 23:18:06 +0800

    Add generate checklist by adding sections

--- src/app/document/document-main/document-main.component.ts
+++ src/app/document/document-main/document-main.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentModelService } from 'src/app/models/document-model.service';
+import { Section } from 'src/app/models/section.interface';
 
 @Component({
   selector: 'app-document-main',
   templateUrl: './document-main.component.html',
   styleUrls: ['./document-main.component.css']
 })
-export class DocumentMainComponent implements OnInit {
+export class DocumentMainComponent {
 
-  constructor() { }
+  constructor(private document: DocumentModelService) { }
 
-  ngOnInit() {
+  getSections(): Section[] {
+    return this.document.model.sections;
   }
-
 }
