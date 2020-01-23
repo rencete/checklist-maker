929d30fc97c07b29544220584e6796cc018530b4
Terence Yu <rencete.workonline@gmail.com>
2020-01-23 20:59:55 +0800

    Add test data and script for running json-server

---
+++ test-data.js
@@ -0,0 +1,226 @@
+module.exports = function () {
+    return {
+        documents: [
+            {
+                id: 1,
+                "title": "Unit 9 Composition Checklist",
+                "editable": true,
+                "showPoints": true,
+                "sections": [
+                    {
+                        "id": 0,
+                        "type": "Questions",
+                        "model": {
+                            "questions": [
+                                {
+                                    "question": "Name:",
+                                    "blankLength": 1
+                                },
+                                {
+                                    "question": "Date:",
+                                    "blankLength": 0
+                                },
+                                {
+                                    "question": "Source Text:",
+                                    "blankLength": 2
+                                }
+                            ],
+                            "title": "Top"
+                        }
+                    },
+                    {
+                        "id": 1,
+                        "type": "Header",
+                        "model": {
+                            "title": "Structure"
+                        }
+                    },
+                    {
+                        "id": 2,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [
+                                {
+                                    "points": 2,
+                                    "text": "Proper format: name, date, double space"
+                                },
+                                {
+                                    "points": 2,
+                                    "text": "Title centered and repeats 1–3 key words from final sentence"
+                                },
+                                {
+                                    "points": 2,
+                                    "text": "Introduction includes attention getter, background information (title, author, publisher, type of story, awards)"
+                                },
+                                {
+                                    "points": 2,
+                                    "text": "Conclusion includes liked? disliked? why?; message; no \"I\""
+                                },
+                                {
+                                    "points": 2,
+                                    "text": "Three body paragraphs follow Story Sequence Chart (Unit 3)"
+                                },
+                                {
+                                    "points": 1,
+                                    "text": "Insert body paragraphs"
+                                },
+                                {
+                                    "points": 1,
+                                    "text": "Each paragraph contains at least 6 sentences"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "Checklist on top, final draft, rough draft, KWO"
+                                }
+                            ],
+                            "columns": 1,
+                            "points": 2,
+                            "pointsType": 1,
+                            "showTitle": false,
+                            "title": "Structure"
+                        }
+                    },
+                    {
+                        "id": 3,
+                        "type": "Header",
+                        "model": {
+                            "title": "Style"
+                        }
+                    },
+                    {
+                        "id": 4,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [
+                                {
+                                    "points": 0,
+                                    "text": "-ly adverb"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "who/which clause"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "strong verb"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "quality adjective"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "www.asia.b clause"
+                                }
+                            ],
+                            "columns": 7,
+                            "points": 2,
+                            "pointsType": 0,
+                            "showTitle": true,
+                            "title": "Dress-Ups (underline)"
+                        }
+                    },
+                    {
+                        "id": 5,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [
+                                {
+                                    "points": 0,
+                                    "text": "[1] subject"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "[2] prepositional"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "[3] -ly adverb"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "[4] -ing"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "[5] clausal (www.asia.b)"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "[6] vss (2–5 words)"
+                                }
+                            ],
+                            "columns": 7,
+                            "points": 2,
+                            "pointsType": 0,
+                            "showTitle": true,
+                            "title": "Sentence Openers (number; one of each as possible)"
+                        }
+                    },
+                    {
+                        "id": 6,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [
+                                {
+                                    "points": 5,
+                                    "text": "Use at least one different decoration per paragraph as possible: question, alliteration, conversation/quotation, simile/metaphor, vss open-close, 3sss"
+                                },
+                                {
+                                    "points": 2,
+                                    "text": "Advanced Style include at least one in your paper: triple (italics or [trip] in margin) or teeter-totter (italics or underline)"
+                                }
+                            ],
+                            "columns": 1,
+                            "points": 2,
+                            "pointsType": 1,
+                            "showTitle": true,
+                            "title": "Decorations (“dec” in margin or italicize)"
+                        }
+                    },
+                    {
+                        "id": 7,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [],
+                            "columns": 1,
+                            "points": 0,
+                            "pointsType": 2,
+                            "showTitle": true,
+                            "title": "CHECK FOR BANNED WORDS (-1 pt. for each use):"
+                        }
+                    },
+                    {
+                        "id": 8,
+                        "type": "Header",
+                        "model": {
+                            "title": "MECHANICS (-1 per error)"
+                        }
+                    },
+                    {
+                        "id": 9,
+                        "type": "Checklist",
+                        "model": {
+                            "items": [
+                                {
+                                    "points": 0,
+                                    "text": "capitalization, spelling, grammar, and punctuation"
+                                },
+                                {
+                                    "points": 0,
+                                    "text": "no “I,” “my,” “we,” “us,” “you”"
+                                }
+                            ],
+                            "columns": 1,
+                            "points": 2,
+                            "pointsType": 1,
+                            "showTitle": false,
+                            "title": "Mechanics"
+                        }
+                    }
+                ],
+                "_nextId": 10
+            }
+        ]
+    }
+}
